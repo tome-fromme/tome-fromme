db7b9ffc7a8e4c4ec0403d60ad28e79406f13327
hyechan <dhgpcks123@naver.com>
2022-06-06 16:57:42 +0900

    feat : role entity 추가

--- src/main/java/tmfm/tomefromme/domain/role/entity/Role.java
+++ src/main/java/tmfm/tomefromme/domain/role/entity/Role.java
@@ -1,4 +1,24 @@
 package tmfm.tomefromme.domain.role.entity;
 
-public class Role {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import tmfm.tomefromme.domain.base.BaseEntity;
+import tmfm.tomefromme.domain.role.constant.RoleType;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Role extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
+
 }
