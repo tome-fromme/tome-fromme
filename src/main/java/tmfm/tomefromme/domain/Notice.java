110cff56f763b56268de667d47a001b1e4e75ce4
hodolee246 <hodolee246@naver.com>
2022-06-06 17:28:20 +0900

    feat : add notice entity

---
+++ src/main/java/tmfm/tomefromme/domain/Notice.java
@@ -0,0 +1,35 @@
+package tmfm.tomefromme.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import tmfm.tomefromme.domain.base.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notice extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long noticeId;
+
+    private Object noticeImgId;
+
+    @Column(name = "notice_title", length = 50)
+    private String noticeTitle;
+
+    @Column(name = "notice_content", length = 100)
+    private String noticeContent;
+
+    @Builder
+    public Notice(Long noticeId, Object noticeImgId, String noticeTitle, String noticeContent) {
+        this.noticeId = noticeId;
+        this.noticeImgId = noticeImgId;
+        this.noticeTitle = noticeTitle;
+        this.noticeContent = noticeContent;
+    }
+
+}
