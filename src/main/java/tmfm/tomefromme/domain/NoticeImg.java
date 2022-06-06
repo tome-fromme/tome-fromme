8f7d58abb7508c8f59f47c63d6aa171628872681
hyechan <dhgpcks123@naver.com>
2022-06-06 17:09:20 +0900

    feat : NoticeImg 엔티티 추가

---
+++ src/main/java/tmfm/tomefromme/domain/NoticeImg.java
@@ -0,0 +1,21 @@
+package tmfm.tomefromme.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import tmfm.tomefromme.domain.base.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class NoticeImg extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String originalImgName;
+    private String savedImgName;
+    private String imgUrl;
+
+}
