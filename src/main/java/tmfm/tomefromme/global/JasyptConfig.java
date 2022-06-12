613fbd307fd41065ffeb96f2f95a93327578383c
hodolee246 <hodolee246@naver.com>
2022-06-12 16:26:34 +0900

    머지 전 커밋 메세지 변경하고 pr 날리기

---
+++ src/main/java/tmfm/tomefromme/global/JasyptConfig.java
@@ -0,0 +1,31 @@
+package tmfm.tomefromme.global;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    private final String key = "MY_JASYPT_KEY";
+
+    @Bean
+    public StringEncryptor stringEncryptor() {
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(key); // 암호화할 때 사용하는 키
+        config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘
+        config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
+        config.setPoolSize("1"); // 인스턴스 pool
+        config.setProviderName("tmfm");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
+        config.setStringOutputType("base64"); //인코딩 방식
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+    
+}
