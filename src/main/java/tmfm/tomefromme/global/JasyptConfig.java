75a6acf843a448b58be3e28277aa5d490e031ace
hodolee246 <hodolee246@naver.com>
2022-06-12 17:05:06 +0900

    feat: db연결 및 정보 암호화 (#10)

--- src/main/java/tmfm/tomefromme/global/JasyptConfig.java
+++ src/main/java/tmfm/tomefromme/global/JasyptConfig.java
@@ -12,13 +12,13 @@ public class JasyptConfig {
     private final String key = "MY_JASYPT_KEY";
 
     @Bean
-    public StringEncryptor stringEncryptor() {
+    public StringEncryptor jasyptStringEncryptor() {
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
         config.setPassword(key); // 암호화할 때 사용하는 키
         config.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘
         config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
         config.setPoolSize("1"); // 인스턴스 pool
-        config.setProviderName("tmfm");
+        config.setProviderName("SunJCE");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
         config.setStringOutputType("base64"); //인코딩 방식
 
