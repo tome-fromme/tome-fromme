63f919d0c6353cbb1e501eb28144d9de97b698fb
hodolee246 <hodolee246@naver.com>
2022-06-18 15:20:54 +0900

    feat: 편지 쓰기 개발 및 예외처리 일부 개발 (#17)

--- src/main/java/tmfm/tomefromme/repository/LetterRepository.java
+++ src/main/java/tmfm/tomefromme/repository/LetterRepository.java
@@ -1,7 +1,9 @@
 package tmfm.tomefromme.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tmfm.tomefromme.domain.letter.entity.Letter;
 
+@Repository
 public interface LetterRepository extends JpaRepository<Letter, Long> {
 }
--- src/main/java/tmfm/tomefromme/service/LetterService.java
+++ src/main/java/tmfm/tomefromme/service/LetterService.java
@@ -5,8 +5,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 import tmfm.tomefromme.domain.letter.entity.Letter;
-import tmfm.tomefromme.global.exception.SomeException;
-import tmfm.tomefromme.global.exception.SomethingResult;
 import tmfm.tomefromme.repository.LetterRepository;
 
 import java.io.File;
@@ -37,7 +35,8 @@ public class LetterService {
             try {
                 Files.copy(multipartFile.getInputStream(), copyOfLocation, StandardCopyOption.REPLACE_EXISTING);
             } catch (IOException e) {
-                throw new SomeException("저장 중 에러발생", 999);
+//                ErrorCode errorCode = new ErrorCode(123, "");
+//                throw new BusinessException(new ErrorCode(000, "e"));
             }
         });
     }
